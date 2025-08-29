52652773ceeb1320fd7a2fc8a0435cd2e07e35f6
bragatheesh07 <bragatheesh@capecomsolutions.com>
2025-08-29 16:19:32 +0530

    fix: add motion div (dev)

--- src/app/_utils/oppurtunities.tsx
+++ src/app/_utils/oppurtunities.tsx
@@ -84,7 +84,7 @@ export default function CareersSection() {
             </p>
 
             {/* CTA Button */}
-            <Button type="button" variant="default" size="lg" className=" px-8 py-6 rounded-full text-lg font-medium transition-all duration-300 hover:scale-105 hover:shadow-lg">
+            <Button type="button" variant="default" size="xl" >
               {careers.exploreBtn}
               <ChevronRight className="ml-2 h-5 w-5 transition-transform" />
             </Button>
