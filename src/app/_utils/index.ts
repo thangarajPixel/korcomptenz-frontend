32bc6faffc4bbc8e40b1c432d3f43f325cb6f441
bragatheesh07 <bragatheesh@capecomsolutions.com>
2025-09-02 12:44:20 +0530

    refactor: code clean up (dev)

--- src/app/_utils/index.ts
+++ src/app/_utils/index.ts
@@ -1,6 +1,6 @@
 import WeAreKorcomptenzSection from './we-are-korcomptenz'
-import InspireSection from './inspire-Section'
-import CareersSection from './oppurtunities';
+import InspireSection from './inspire-section-one'
+import CareersSection from './opportunities';
 import ServicesSection from './services-section';
 
 export { WeAreKorcomptenzSection, InspireSection, CareersSection, ServicesSection };
