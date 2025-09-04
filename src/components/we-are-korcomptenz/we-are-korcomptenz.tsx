fefc57b12c3bc0b7f2fdd3ddfa0c5dbfeae26994
Jameel21 <jameel@capecomsolutions.com>
2025-09-04 11:40:47 +0530

    we are sec mob (dev)

--- src/components/we-are-korcomptenz/we-are-korcomptenz.tsx
+++ src/components/we-are-korcomptenz/we-are-korcomptenz.tsx
@@ -3,32 +3,32 @@ import React from "react";
 
 const WeAreKorcomptenzSection = () => {
   return (
-    <div className="container-md mt-24">
-      <div className="grid lg:grid-cols-[1fr_2fr] gap-32 items-start mb-16">
+    <div className="container-md md:mt-24">
+      <div className="grid lg:grid-cols-[1fr_2fr] gap-10 md:gap-32 items-start mb-10 md:mb-16">
         {/* Left Column - Company Name */}
         <div className="lg:w-full">
           <h1 className="text-2xl lg:text-4xl font-bold leading-tight">
             We are
             <br />
-            <span className="text-xl lg:text-3xl">Korcomptenz</span>
+            <span className="text-2xl lg:text-3xl font-semibold">Korcomptenz</span>
           </h1>
         </div>
 
         {/* Right Column - Description */}
         <div className="space-y-5">
-          <p className="font-outfit font-normal text-lg leading-[34px] tracking-[0%] text-pretty whitespace-pre-wrap">
+          <p className="font-outfit font-normal text-xs md:text-lg leading-[24px] md:leading-[34px] tracking-[0%] text-pretty whitespace-pre-wrap">
             {`We lead with expertise - in technology and domain to deliver solutions that align with your business\ngoals.`}
           </p>
-          <p className="font-outfit font-normal text-lg leading-[34px] tracking-[0%] text-pretty whitespace-pre-wrap">
+          <p className="font-outfit font-normal text-xs md:text-lg leading-[24px] md:leading-[34px] tracking-[0%] text-pretty whitespace-pre-wrap">
             {`We leverage our experience and robust partner\necosystem to elevate your processes, powering your\ntransformation journey toward impactful growth`}
           </p>
         </div>
       </div>
 
-      <div className="relative">
+      <div className="relative m-1 md:m-0">
         <KorcomptenzImage
           src="/assets/home/video-thumb.jpg"
-          className="w-full h-[500px] object-cover rounded-4xl"
+          className="w-full h-[280px] md:h-[500px] object-cover  rounded-4xl"
           alt="video-thumb"
           width={1112}
           height={500}
