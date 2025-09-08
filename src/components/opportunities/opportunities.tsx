9fd66fdb51f783813ca934c73679dc4c91e6c2d8
bragatheesh07 <bragatheesh@capecomsolutions.com>
2025-09-08 18:51:21 +0530

    fix: responsie (dev)

--- src/components/opportunities/opportunities.tsx
+++ src/components/opportunities/opportunities.tsx
@@ -8,18 +8,18 @@ import KorcomptenzImage from "../korcomptenz-image";
 export default function Opportunities() {
   const careers = jsonData.careers;
   return (
-    <section className="bg-white px-4 sm:px-6 lg:px-8 mb-6">
+    <section className="bg-white  px-4 sm:px-6 lg:px-8 mb-6">
       <div className="max-w-7xl mx-auto">
-        <div className="grid grid-cols-1 lg:grid-cols-2 md:gap-12 gap-4 items-center transition-all duration-1000 group">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center group">
           {/* Left side - Image with geometric elements */}
           <div className="relative">
             {/* Blue geometric arrows */}
-            <div className="absolute top-2 md:top-8 left-10 md:left-6 z-10 group-hover:left-24 transition-all duration-1000">
+            <div className="absolute top-8 left-6 z-10 group-hover:left-24 transition-all duration-1000">
               <div className="flex space-x-1 w-72">
                 <Image
                   src="/assets/home/arrow.png"
                   alt="arrow"
-                  className="md:w-full w-2/4 h-auto rounded-lg"
+                  className="w-full h-auto rounded-lg"
                   width={500}
                   height={300}
                 />
@@ -41,54 +41,40 @@ export default function Opportunities() {
           {/* Right side - Content */}
           <div className="space-y-8 mx-auto">
             {/* Header text with profile images */}
-            <div className="relative   hidden lg:block">
-              <div className="text-xl font-bold leading-tight w-full">
-                <div className="flex flex-row  justify-between ">
-                  <span className="text-[#22282E] transition-all duration-500 group-hover:-translate-x-2 text-lg lg:text-3xl font-semibold lg:me-[50px]">
-                    Ready to
-                  </span>
-                  <section className="flex flex-row  gap-2">
+            <div className="relative hidden lg:block group-hover:-translate-x-5 group-hover:scale-102 transition-all ">
+              <div className="text-xl lg:text-5xl font-bold leading-tight w-full">
+                <div className="flex flex-row items-center justify-between">
+                  <span className="text-[#22282E] lg:-ml-56 text-lg lg:text-3xl font-semibold lg:me-20">Ready to </span>
+                  <section className="flex flex-row items-center gap-2">
                     <div>
-                      <Image
+                      <img
                         src="/assets/home/profile-1.png"
                         alt="Team member"
-                        className="lg:w-[100px] lg:h-[100px] w-16 h-16 transition-all duration-500 group-hover:scale-105 rounded-full border-4 border-white shrink-0"
-                        width={1000}
-                        height={1000}
+                        className="lg:w-[100px] lg:h-[100px] w-16 h-16 rounded-full border-4 border-white shrink-0"
                       />
                     </div>
-                    <span className="text-[#22282E] transition-all duration-500 group-hover:-translate-x-2 text-lg lg:text-3xl font-semibold">
-                      build an
-                    </span>
+                    <span className="text-[#22282E] text-lg lg:text-3xl  font-semibold">build an</span>
                   </section>
                   <div className="ps-10">
-                    <KorcomptenzImage
+                    <img
                       src="/assets/home/profile-3.png"
                       alt="Team member"
-                      className="w-16 h-16 lg:w-[100px] lg:h-[100px] transition-all duration-500 group-hover:scale-105 rounded-full border-4 border-white shrink-0"
-                      width={1000}
-                      height={1000}
+                      className="w-16 h-16 lg:w-[100px] lg:h-[100px] rounded-full border-4 border-white shrink-0"
                     />
                   </div>
                 </div>
 
-                <div className="flex flex-row  mt:[40px]">
-                  <span className="text-primary text-lg  transition-all duration-500 group-hover:-translate-x-2 lg:text-3xl lg:me-38 font-semibold">
-                    exceptional{" "}
-                  </span>
-                  <section className="flex flex-row  gap-2 lg:-ml-12">
-                    <div>
-                      <KorcomptenzImage
+                <div className="flex flex-row items-center  mt:[40px]">
+                  <span className="text-primary text-lg lg:-ml-42 lg:text-3xl lg:me-48 font-semibold">exceptional </span>
+                  <section className="flex flex-row items-center gap-2 lg:-ml-12">
+                    <div >
+                      <img
                         src="/assets/home/profile-2.png"
                         alt="Team member"
-                        className="lg:w-[100px] lg:h-[100px] w-16 h-16 rounded-full border-4 border-white shrink-0 transition-all duration-500 group-hover:scale-105"
-                        width={1000}
-                        height={1000}
+                        className="lg:w-[100px] lg:h-[100px] w-16 h-16  rounded-full border-4 border-white shrink-0"
                       />
                     </div>
-                    <span className="text-primary transition-all duration-500 group-hover:-translate-x-2 text-lg lg:text-3xl font-semibold">
-                      career?
-                    </span>
+                    <span className="text-primary text-lg lg:text-3xl font-semibold">career?</span>
                   </section>
                 </div>
               </div>
@@ -145,16 +131,17 @@ export default function Opportunities() {
               </div>
             </div>
             {/* Description text */}
-            <p className="text-sm text-[#22282E] leading-[28px] max-w-lg lg:ms-[-30px]">
+            <p className="text-md text-[#22282E] leading-[28px] max-w-lg lg:ms-[-30px]">
               {careers.description}
             </p>
 
             {/* CTA Button */}
             <Button
               type="button"
+              arrow={true}
               variant="default"
               size="xl"
-              arrow={true}
+              className="text-sm lg:ms-[-30px]"
             >
               {careers.exploreBtn}
             </Button>
