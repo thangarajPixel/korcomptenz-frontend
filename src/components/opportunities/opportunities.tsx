576802966f7a173df36eb8f066e2276c9f50ebbf
Jameel21 <jameel@capecomsolutions.com>
2025-09-08 17:27:23 +0530

    code (kor-com/home)

--- src/components/opportunities/opportunities.tsx
+++ src/components/opportunities/opportunities.tsx
@@ -10,11 +10,11 @@ export default function Opportunities() {
   return (
     <section className="bg-white px-4 sm:px-6 lg:px-8 mb-6">
       <div className="max-w-7xl mx-auto">
-        <div className="grid grid-cols-1 lg:grid-cols-2 md:gap-12 gap-4 items-center group">
+        <div className="grid grid-cols-1 lg:grid-cols-2 md:gap-12 gap-4 items-center transition-all duration-1000 group">
           {/* Left side - Image with geometric elements */}
           <div className="relative">
             {/* Blue geometric arrows */}
-            <div className="absolute top-2 md:top-8 left-2 md:left-6 z-10 group-hover:left-24 transition-all duration-1000">
+            <div className="absolute top-2 md:top-8 left-10 md:left-6 z-10 group-hover:left-24 transition-all duration-1000">
               <div className="flex space-x-1 w-72">
                 <Image
                   src="/assets/home/arrow.png"
@@ -41,8 +41,8 @@ export default function Opportunities() {
           {/* Right side - Content */}
           <div className="space-y-8 mx-auto">
             {/* Header text with profile images */}
-            <div className="relative group-hover:-translate-x-5 group-hover:scale-102 transition-all hidden lg:block">
-              <div className="text-xl  font-bold leading-tight w-full">
+            <div className="relative transition-all duration-500 group-hover:-translate-x-5 group-hover:scale-105 hidden lg:block">
+              <div className="text-xl font-bold leading-tight w-full">
                 <div className="flex flex-row items-center justify-between">
                   <span className="text-[#22282E] text-lg lg:text-3xl font-semibold lg:me-20">
                     Ready to{" "}
@@ -57,7 +57,7 @@ export default function Opportunities() {
                         height={1000}
                       />
                     </div>
-                    <span className="text-[#22282E] text-lg lg:text-3xl  font-semibold">
+                    <span className="text-[#22282E] text-lg lg:text-3xl font-semibold">
                       build an
                     </span>
                   </section>
@@ -72,8 +72,8 @@ export default function Opportunities() {
                   </div>
                 </div>
 
-                <div className="flex flex-row items-center  mt:[40px]">
-                  <span className="text-primary text-lg lg:-ml-42 lg:text-3xl lg:me-48 font-semibold">
+                <div className="flex flex-row items-center mt:[40px]">
+                  <span className="text-primary text-lg lg:-ml-38 lg:text-3xl lg:me-38 font-semibold">
                     exceptional{" "}
                   </span>
                   <section className="flex flex-row items-center gap-2 lg:-ml-12">
@@ -81,7 +81,7 @@ export default function Opportunities() {
                       <KorcomptenzImage
                         src="/assets/home/profile-2.png"
                         alt="Team member"
-                        className="lg:w-[100px] lg:h-[100px] w-16 h-16  rounded-full border-4 border-white shrink-0"
+                        className="lg:w-[100px] lg:h-[100px] w-16 h-16 rounded-full border-4 border-white shrink-0"
                         width={1000}
                         height={1000}
                       />
@@ -94,10 +94,10 @@ export default function Opportunities() {
               </div>
             </div>
             {/* Header text with profile images responsive */}
-            <div className="relative group-hover:-translate-x-5 group-hover:scale-102 transition-all lg:hidden ">
+            <div className="relative transition-all duration-500 group-hover:-translate-x-5 group-hover:scale-105 lg:hidden">
               <div className="text-xl lg:text-5xl font-bold leading-tight w-full">
                 <div className="flex flex-row items-center justify-between">
-                  <span className="text-[#22282E]  text-xl font-semibold ">
+                  <span className="text-[#22282E] text-xl font-semibold">
                     Ready to{" "}
                   </span>
 
@@ -110,15 +110,13 @@ export default function Opportunities() {
                       height={1000}
                     />
                   </div>
-                  <span className="text-[#22282E] text-xl  font-semibold">
+                  <span className="text-[#22282E] text-xl font-semibold">
                     build an
                   </span>
-
-
                 </div>
 
-                <div className="flex flex-row justify-between items-center  mt:[40px]">
-                  <div >
+                <div className="flex flex-row justify-between items-center mt:[40px]">
+                  <div>
                     <KorcomptenzImage
                       src="/assets/home/profile-3.png"
                       alt="Team member"
@@ -127,7 +125,7 @@ export default function Opportunities() {
                       height={1000}
                     />
                   </div>
-                  <span className="text-primary text-xl  font-semibold">
+                  <span className="text-primary text-xl font-semibold">
                     exceptional{" "}
                   </span>
 
@@ -137,10 +135,9 @@ export default function Opportunities() {
                       alt="Team member"
                       width={1000}
                       height={1000}
-                      className="lg:w-[100px] lg:h-[100px] w-16 h-16  rounded-full border-4 border-white shrink-0"
+                      className="lg:w-[100px] lg:h-[100px] w-16 h-16 rounded-full border-4 border-white shrink-0"
                     />
                   </div>
-
                 </div>
                 <span className="text-primary text-xl font-semibold">
                   career?
