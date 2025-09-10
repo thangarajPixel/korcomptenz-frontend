50a38edd950a0fcaede79cfbade71ad6b9b85bb4
PadmakumarC739 <padmakumar@capecomsolutions.com>
2025-09-10 15:41:41 +0530

    Update mega-menu-content.tsx (kor-cpm/json-update)

--- src/components/layout/_utils/mega-menu/mega-menu-content.tsx
+++ src/components/layout/_utils/mega-menu/mega-menu-content.tsx
@@ -1,37 +1,38 @@
 import { AnimatePresence, motion } from 'framer-motion';
-import React from 'react'
+import React from 'react';
 
-import ServicesMenu from './_utils/service-menu';
-
-
+import IndustriesMenu from './_utils/industries-menu';
+import ServicesMenu from './_utils/service-menu copy';
 
 const MegaMenuContent = ({ activeTab }: { activeTab: string }) => {
-  // const activeSection = contentSections[1];
+  console.log("activeTab", activeTab);
+
   return (
-    <AnimatePresence>
+    <AnimatePresence mode="wait">
       {activeTab && (
         <motion.div
+          key={activeTab} 
           initial={{ opacity: 0, y: -20 }}
           animate={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0, y: -20 }}
           transition={{ duration: 0.3, ease: "easeOut" }}
           className="absolute top-full left-0 w-full bg-white border-b border-t border-border shadow-lg z-50"
         >
-          <div className="max-w-7xl mx-auto  py-8">
+          <div className="w-full px-10 mx-auto py-10">
             <motion.div
+              key={activeTab + "-inner"}
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               transition={{ delay: 0.1, duration: 0.3 }}
             >
-
-              <ServicesMenu />
-
+              {activeTab === "Services" && <ServicesMenu />}
+              {activeTab === "Industries" && <IndustriesMenu />}
             </motion.div>
           </div>
         </motion.div>
       )}
     </AnimatePresence>
-  )
-}
+  );
+};
 
-export default MegaMenuContent
+export default MegaMenuContent;
