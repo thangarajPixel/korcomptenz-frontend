104cf48f2a6a004031e46eaba418a909f29d60fd
bragatheesh07 <bragatheesh@capecomsolutions.com>
2025-09-22 12:52:27 +0530

    build: added collapse (kor-com/service)

--- src/components/faq-section/faq-section.tsx
+++ src/components/faq-section/faq-section.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState } from "react";
-import { ChevronDown, ChevronUp } from "lucide-react";
+import { ChevronDownIcon } from "lucide-react";
+import { Accordion, AccordionItem, AccordionTrigger, AccordionContent } from "../ui/accordion";
 
 const FaqSection = ({ faqData }: { faqData: FaqSectionType }) => {
   const [activeId, setActiveId] = useState<number | null>(null);
@@ -14,32 +15,32 @@ const FaqSection = ({ faqData }: { faqData: FaqSectionType }) => {
       <h1 className="text-6xl md:text-9xl font-semibold text-foreground mb-4">
         {faqData.title}
       </h1>
-
-      {faqData.faq.map((item) => (
-        <div
-          key={item.id}
-          className={`rounded-2xl p-4 md:p-6 my-8 ${activeId === item.id ? "bg-light-gray" : "bg-custom-gray-8"
-            }`}
-        >
-          <button
-            className="text-lg md:text-5xl font-semibold w-full text-left flex justify-between items-start text-foreground"
-            onClick={() => handleToggle(item.id)}
+      <Accordion
+        type="single"
+        collapsible
+        className="w-full"
+      >
+        {faqData.faq.map((item) => (
+          <AccordionItem
+            key={`faq-item-${item.id}`}
+            value={`item-${item.id}`}
+            className={`rounded-2xl p-4 md:p-6 my-6 data-[state=open]:bg-light-gray bg-custom-gray-8`}
           >
-            <span>{item.title}</span>
-            <span className="p-2 rounded-full bg-primary">
-              {activeId === item.id ? (
-                <ChevronUp color="white" />
-              ) : (
-                <ChevronDown color="white" />
-              )}
-            </span>
-          </button>
+            <AccordionTrigger
+              className="cursor-pointer text-lg md:text-5xl font-semibold w-full text-left flex justify-between items-start text-foreground"
+              onClick={() => handleToggle(item.id)}
+            >
+              <span>{item.title}</span>
+              <ChevronDownIcon className="size-10 p-2 text-white rounded-full bg-primary pointer-events-none  shrink-0 translate-y-0.5 transition-transform duration-200" />
+            </AccordionTrigger>
+            <AccordionContent className="flex flex-col gap-4 text-balance">
+              <p className="mt-2 text-lg text-answer-color">{item.description}</p>
+            </AccordionContent>
+          </AccordionItem>
+        ))}
 
-          {activeId === item.id && (
-            <p className="mt-2 text-lg text-answer-color">{item.description}</p>
-          )}
-        </div>
-      ))}
+      </Accordion>
+
     </div>
   );
 };
