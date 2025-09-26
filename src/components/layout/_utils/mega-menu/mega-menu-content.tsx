824954770f5fd57e80d2fc67581a21665ee2a473
PadmakumarC739 <padmakumar@capecomsolutions.com>
2025-09-26 12:25:39 +0530

    updated (dev)

--- src/components/layout/_utils/mega-menu/mega-menu-content.tsx
+++ src/components/layout/_utils/mega-menu/mega-menu-content.tsx
@@ -33,7 +33,7 @@ const MegaMenuContent = ({
             >
               {activeTab === "Services" && <ServicesMenu data={data} />}
               {activeTab === "Industries" && <IndustriesMenu data={data} />}
-              {activeTab === "Ecosystems" && <EcosystemMenu />}
+              {activeTab === "Ecosystems" && <EcosystemMenu data={data} />}
               {activeTab === "Insights" && <InsightsMenu data={data} />}
               {activeTab === "About Us" && <AboutMenu data={data} />}
             </motion.div>
