9c0b82426e3c25ce3d1a91f870a6bd3caebc9164
bragatheesh07 <bragatheesh@capecomsolutions.com>
2025-10-03 17:22:03 +0530

    build: base tanstack query (kor-com/case-studies)

--- src/components/providers/providers.tsx
+++ src/components/providers/providers.tsx
@@ -2,17 +2,24 @@
 
 import { ThemeProvider } from "./_utils/theme-provider";
 import { Toaster } from "../ui/sonner";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/lib/utils";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="light"
-      // enableSystem
-      disableTransitionOnChange
-    >
-      {children}
-      <Toaster richColors />
-    </ThemeProvider>
+    <NuqsAdapter>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          // enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+          <Toaster richColors />
+        </ThemeProvider>
+      </QueryClientProvider >
+    </NuqsAdapter>
   );
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,16 @@
+import { QueryClient } from "@tanstack/react-query";
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-	return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs));
 }
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      gcTime: 1000 * 60 * 120, // 2 hours
+      refetchOnWindowFocus: false,
+    },
+  },
+});
