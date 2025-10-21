e65c816fb74957c77da65ce5c15a4505ac0ef1b6
bragatheesh07 <bragatheesh@capecomsolutions.com>
2025-10-21 11:15:31 +0530

    fix: code clean up (kor-com/case-studies)

--- src/components/ui/dangerous-html.tsx
+++ src/components/ui/dangerous-html.tsx
@@ -1,10 +1,13 @@
+'use client'
 import React from 'react'
 import "./ui.css";
 import { cn } from '@/lib/utils';
-import DOMPurify from 'dompurify';
+import * as DOMPurifyModule from 'dompurify';
+
+const DOMPurify = DOMPurifyModule.default || DOMPurifyModule;
 
 const DangerousHtml = ({ html, className }: { html: string, className?: string }) => {
-  const clean = DOMPurify.sanitize(html);
+  const clean = DOMPurify?.sanitize(html || "");
   return (
     <div className={cn("rich-text", className)} dangerouslySetInnerHTML={{ __html: clean }} />
   )
