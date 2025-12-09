868c7369469f16ca71638e85d197e1c2db7b59c0
PadmakumarC739 <padmakumar@capecomsolutions.com>
2025-12-09 12:34:37 +0530

    Updated promise (dev)

--- src/components/stretchable-section/_utils/stretchable-card.tsx
+++ src/components/stretchable-section/_utils/stretchable-card.tsx
@@ -1,29 +1,29 @@
-import { Button } from '@/components/ui/button'
-import KorcomptenzImage from '@/components/korcomptenz-image'
-import { cn } from '@/lib/utils'
-import Link from 'next/link';
-import { DangerousHtml } from '@/components/ui/dangerous-html';
+import { Button } from "@/components/ui/button";
+
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { DangerousHtml } from "@/components/ui/dangerous-html";
 
 const StretchableSectionCard = ({
-  open,
   props,
   data,
-  image = "https://aue2kormlworkspacetest01.blob.core.windows.net/pixelteam-datastorage/lets_drive_fdc0c33e0c.png"
+  image = "https://aue2kormlworkspacetest01.blob.core.windows.net/pixelteam-datastorage/lets_drive_fdc0c33e0c.png",
 }: {
-  open: boolean;
   props: React.HTMLAttributes<HTMLDivElement>;
   image?: ImageType | string;
-  data: StretchableSectionType['list'][number];
+  data: StretchableSectionType["list"][number];
 }) => {
-  const { ...rest } = props
+  const { ...rest } = props;
   const backgroundImage =
-    typeof image === 'string' ? image : (image as ImageType)?.url ?? '';
-
+    typeof image === "string" ? image : (image as ImageType)?.url ?? "";
 
   return (
     <div
       {...rest}
-      className={cn(`bg-light-gray rounded-4xl relative overflow-hidden min-h-80 flex flex-row`, props.className)}
+      className={cn(
+        `bg-light-gray rounded-4xl relative overflow-hidden min-h-80 flex flex-row`,
+        props.className
+      )}
       style={{
         backgroundImage: `url(${backgroundImage})`,
         backgroundRepeat: "no-repeat",
@@ -34,8 +34,10 @@ const StretchableSectionCard = ({
       {/* Overlay to reduce background image opacity */}
       <div className="absolute inset-0 bg-light-gray/60 z-0"></div>
 
-      <div className='pl-8 py-8 lg:pl-10 lg:py-10 flex flex-col gap-3 flex-1 pr-8 relative z-10'>
-        <h2 className="text-2xl lg:text-3xl font-bold text-gray-900">{data?.title}</h2>
+      <div className="pl-8 py-8 lg:pl-10 lg:py-10 flex flex-col gap-3 flex-1 pr-8 relative z-10">
+        <h2 className="text-2xl lg:text-3xl font-bold text-gray-900">
+          {data?.title}
+        </h2>
         <div className="flex-1">
           <DangerousHtml
             html={data?.description}
@@ -44,16 +46,14 @@ const StretchableSectionCard = ({
         </div>
         <div className="z-10">
           <Link href={data?.link || "#"}>
-            <Button size='xl' arrow={true}>
+            <Button size="xl" arrow={true}>
               {data?.buttonText}
             </Button>
           </Link>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default StretchableSectionCard
-
-
+export default StretchableSectionCard;
