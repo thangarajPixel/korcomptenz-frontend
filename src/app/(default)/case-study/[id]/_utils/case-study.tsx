b0c1a0aeedde3e8ce367462e23ae7a5001c39013
bragatheesh07 <bragatheesh@capecomsolutions.com>
2025-12-18 15:39:38 +0530

    fix: related product (kor-comp/hot-fix)

--- src/app/(default)/case-study/[id]/_utils/case-study.tsx
+++ src/app/(default)/case-study/[id]/_utils/case-study.tsx
@@ -25,37 +25,36 @@ const CaseStudy = ({ data, essential, }: { data: CaseStudySingleData; essential:
         <ClientTestimonial data={data?.testimonials} />
       )}
       <div className={cn("h-1 container-md my-20 bg-primary ")} />
-      <div className="container-md">
-        <h2
-          id="insights-heading"
-          className="text-pretty lg:text-7xl text-6xl font-semibold text-primary break-words md:text-7xl "
-        >
-          {essential?.relatedCase?.title || "Explore more success stories"}
-        </h2>
-        <div className="grid grid-cols-12 gap-6 mb-8 md:py-10">
-          {data?.relatedCaseStudies?.length > 0 &&
-            data?.relatedCaseStudies?.map((item) => (
+      {data?.relatedCaseStudies?.length > 0 &&
+        <div className="container-md">
+          <h2
+            id="insights-heading"
+            className="text-pretty lg:text-7xl text-6xl font-semibold text-primary break-words md:text-7xl "
+          >
+            {essential?.relatedCase?.title || "Explore more success stories"}
+          </h2>
+          <div className="grid grid-cols-12 gap-6 mb-8 md:py-10">
+            {data?.relatedCaseStudies?.map((item) => (
               <div
                 key={`related-case-study-${item.id}`}
                 className="col-span-12 md:col-span-6 lg:col-span-4"
               >
                 <CaseStudyCard data={item} />
-              </div>
-            ))}
+              </div>))}
+          </div>
+          <div className="flex justify-center">
+            <ButtonLink
+              link="/client-success"
+              buttonProps={{
+                size: "xl",
+                arrow: true,
+              }}
+            >
+              {essential?.relatedCase?.buttonText || "View all case studies"}
+            </ButtonLink>
+          </div>
         </div>
-        <div className="flex justify-center">
-          <ButtonLink
-            link="/client-success"
-            buttonProps={{
-              size: "xl",
-              arrow: true,
-            }}
-          >
-            {essential?.relatedCase?.buttonText || "View all case studies"}
-          </ButtonLink>
-        </div>
-      </div>
-
+      }
       <CaseStudyForm data={data} essential={essential} />
     </React.Fragment>
   );
