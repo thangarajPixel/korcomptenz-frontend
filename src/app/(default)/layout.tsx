4297ce96463bd7a5e833cdf870acee0694524dc6
bragatheesh07 <bragatheesh@capecomsolutions.com>
2025-12-30 13:06:51 +0530

    aaa (dev)

--- src/app/(default)/layout.tsx
+++ src/app/(default)/layout.tsx
@@ -1,7 +1,7 @@
 import Layout from "@/components/layout"
 import React from "react"
 
-export default function DefaultLayout({ children }: { children: React.ReactNode }) {
+export default async function DefaultLayout({ children }: { children: React.ReactNode }) {
   return <Layout>
     {children}
   </Layout>
