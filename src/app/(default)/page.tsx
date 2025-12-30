13a7d85725b373d6791a0df779747bad125c3e96
bragatheesh07 <bragatheesh@capecomsolutions.com>
2025-12-30 12:21:14 +0530

    build check (dev)

--- src/app/(default)/page.tsx
+++ src/app/(default)/page.tsx
@@ -1,25 +1,26 @@
-import GlobalPage from "@/components/global-page";
+// import GlobalPage from "@/components/global-page";
 import { cn } from "@/lib/utils";
-import { getHomeService } from "@/services";
+// import { getHomeService } from "@/services";
 import { APP_CONFIG } from "@/utils/app-config";
-import { cache } from "react";
+// import { cache } from "react";
 
 export const dynamic = "force-dynamic";
 
-const getHomeServiceCache = cache(getHomeService);
+// const getHomeServiceCache = cache(getHomeService);
 
-export async function generateMetadata() {
-  const data = await getHomeServiceCache();
-  return {
-    title: data?.seo?.title || "Home",
-    description: data?.seo?.description || "",
-  }
-}
+// export async function generateMetadata() {
+//   const data = await getHomeServiceCache();
+//   return {
+//     title: data?.seo?.title || "Home",
+//     description: data?.seo?.description || "",
+//   }
+// }
 export default async function Home() {
-  const data = await getHomeServiceCache();
+  // const data = await getHomeServiceCache();
   return (
     <div className={cn("flex flex-col pb-10 md:pb-24", APP_CONFIG.OVERALL_GAP)}>
-      <GlobalPage data={data?.list} />
+      {/* <GlobalPage data={data?.list} /> */}
+      jj
     </div>
   );
 }
