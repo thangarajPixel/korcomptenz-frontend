b33ce90f96a88eb4f51acfe187039bebd6c1781a
bragatheesh07 <bragatheesh@capecomsolutions.com>
2025-12-30 12:57:21 +0530

    fix: update this (dev)

--- src/app/(default)/page.tsx
+++ src/app/(default)/page.tsx
@@ -1,29 +1,26 @@
-"use client";
-// import GlobalPage from "@/components/global-page";
-// import { cn } from "@/lib/utils";
+import GlobalPage from "@/components/global-page";
+import { cn } from "@/lib/utils";
 import { getHomeService } from "@/services";
-// import { APP_CONFIG } from "@/utils/app-config";
-import { cache, useEffect } from "react";
+import { APP_CONFIG } from "@/utils/app-config";
+import { cache } from "react";
 
-// export const dynamic = "force-dynamic";
+export const dynamic = "force-dynamic";
 
 const getHomeServiceCache = cache(getHomeService);
 
-// export async function generateMetadata() {
-//   const data = await getHomeServiceCache();
-//   return {
-//     title: data?.seo?.title || "Home",
-//     description: data?.seo?.description || "",
-//   }
-// }
-export default function Home() {
-  useEffect(() => {
-    getHomeServiceCache();
-  }, []);
+export async function generateMetadata() {
+  const data = await getHomeServiceCache();
+  return {
+    title: data?.seo?.title || "Home",
+    description: data?.seo?.description || "",
+  }
+}
+export default async function Home() {
+  const data = await getHomeServiceCache();
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={cn("flex flex-col pb-10 md:pb-24", APP_CONFIG.OVERALL_GAP)}>
+      <GlobalPage data={data?.list} />
     </div>
-  )
+  );
 }
 
--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,17 +1,17 @@
 import React from "react";
-// import { Header, Footer } from "./_utils";
-// import { getLayoutService } from "@/services";
+import { Header, Footer } from "./_utils";
+import { getLayoutService } from "@/services";
 import ScrollTopButton from "../ui/scroll-top";
 
 export const dynamic = "force-dynamic";
 
 const Layout = async ({ children }: { children: React.ReactNode }) => {
-  // const response = await getLayoutService();
+  const response = await getLayoutService();
   return (
     <div className="flex min-h-svh  flex-col">
-      {/* <Header data={response} /> */}
+      <Header data={response} />
       <main className="flex-1">{children}</main>
-      {/* <Footer data={response} /> */}
+      <Footer data={response} />
       <ScrollTopButton />
     </div>
   );
