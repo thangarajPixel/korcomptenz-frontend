e94aabe1944db9540b59b9cd36081ad37507607b
PadmakumarC739 <padmakumar@capecomsolutions.com>
2026-01-05 19:02:24 +0530

    Update page.tsx (main)

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -1,7 +1,11 @@
-
+"use client";
+import { getCaseStudyPage } from "@/services";
 import ThankYouPage from "../thank-you";
 
 const Page = () => {
+  const data = getCaseStudyPage();
+
+  if (!data) return null;
   return <ThankYouPage />;
 };
 
