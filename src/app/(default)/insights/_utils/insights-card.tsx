a31d707778f7124133ddd334c5c25750076a20f5
PadmakumarC739 <padmakumar@capecomsolutions.com>
2026-01-05 20:52:21 +0530

    Revert "updated (main)"
    This reverts commit f2ba4c9bf6ca51f64e41ec6c9c120476a7e55a28. (dev)

--- src/app/(default)/insights/_utils/insights-card.tsx
+++ src/app/(default)/insights/_utils/insights-card.tsx
@@ -4,7 +4,7 @@ import KorcomptenzImage from "@/components/korcomptenz-image";
 import ButtonLink from "@/components/ui/button-link";
 import { cn } from "@/lib/utils";
 import React from "react";
-import { motion } from "framer-motion";
+import { motion } from "motion/react";
 
 export function InsightCard({ data }: { data: CaseStudyData & { category?: { slug: string } } }) {
 
