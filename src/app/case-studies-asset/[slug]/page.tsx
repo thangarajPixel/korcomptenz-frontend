2e08b5b9a146aa540da4d3c19adaba0eb8b1311c
PadmakumarC739 <padmakumar@capecomsolutions.com>
2026-01-30 23:04:45 +0530

    y (main)

--- src/app/case-studies-asset/[slug]/page.tsx
+++ src/app/case-studies-asset/[slug]/page.tsx
@@ -1,7 +1,7 @@
-import InsightCasePdfSection from "../insight-case";
-type Props = {
-  params: Promise<{ slug: string }>;
-};
-export default async function Page({ params }: Props) {
-  return <InsightCasePdfSection params={params} />;
-}
+// import InsightCasePdfSection from "../insight-case";
+// type Props = {
+//   params: Promise<{ slug: string }>;
+// };
+// export default async function Page({ params }: Props) {
+//   return <InsightCasePdfSection params={params} />;
+// }
