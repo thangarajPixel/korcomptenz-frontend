9cd8bcbeaf007d0da46185086c89edea90112e01
Ajith M <ajith.m@capecomsolutions.com>
2026-02-25 18:28:19 +0530

    Page insight changes (page-insights)

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  compress: true,
+  poweredByHeader: false,
+  productionBrowserSourceMaps: false,
   images: {
     remotePatterns: [
       {
