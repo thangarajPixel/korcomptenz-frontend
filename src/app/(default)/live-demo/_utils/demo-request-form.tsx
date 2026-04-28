8b9e62f7173a3bacd3bdda1e1b570dd04079c17b
PadmakumarC739 <padmakumar@capecomsolutions.com>
2026-04-28 12:26:42 +0530

    Update demo-request-form.tsx (kor-h2h3-isuue)

--- src/app/(default)/live-demo/_utils/demo-request-form.tsx
+++ src/app/(default)/live-demo/_utils/demo-request-form.tsx
@@ -98,9 +98,11 @@ const DemoRequestForm = ({
     >
       <div className="grid rounded-4xl shadow-2xl p-10 gap-y-2 w-full md:w-3/4">
         {/* Name + Email */}
-        <h3 className="text-5xl font-semibold text-center text-foreground mb-5">
-          {form?.title || "Reserve My Spot"}
-        </h3>
+       
+          <h3 className="text-5xl font-semibold text-center text-foreground mb-5">
+            {form?.title || "Reserve My Spot"}
+          </h3>
+      
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 ">
           <Input
             control={control}
