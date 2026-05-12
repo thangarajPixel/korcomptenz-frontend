2887db67a49d60ab7ffb1781c9625368f3007565
korcomptenzangeline <137047763+korcomptenzangeline@users.noreply.github.com>
2026-05-12 17:53:44 +0530

    New Insights Battlecars Creation (kordev/insights-battlecards)

---
+++ src/app/battlecard/[slug]/page.tsx
@@ -0,0 +1,25 @@
+import { InsightPdfSection } from "@/components/pdf-view-section";
+import { getBlogPage } from "@/services";
+import { cache } from "react";
+import { generatePageMetadata } from "@/utils/metadata";
+
+type Props = {
+  params: Promise<{ slug: string }>;
+};
+
+const getPageServiceCache = cache(getBlogPage);
+
+export async function generateMetadata({ params }: Props) {
+  const { slug } = await params;
+  const data = await getPageServiceCache({ id: slug });
+  return generatePageMetadata({
+    title: data?.seo?.title,
+    description: data?.seo?.description,
+    path: `/battlecard/${slug}`,
+    image: data?.heroSection?.image?.url,
+  });
+}
+
+export default async function Page({ params }: Props) {
+  return <InsightPdfSection params={params} />;
+}
