b8c8be4ed07723f1824bdca9c81b10755d4408e3
Arasu-7 <malarasu@capecomsolutions.com>
2026-05-27 13:14:13 +0530

    update cloud warning (kor-new-cloud)

--- src/components/cloud-warning/cloud-warning.tsx
+++ src/components/cloud-warning/cloud-warning.tsx
@@ -3,6 +3,7 @@
 import { DangerousHtml } from "../ui/dangerous-html";
 
 const CloudWarning = ({ data }: { data: CloudWarningType }) => {
+
   return (
     <section className="container-md ">
       {/* Heading */}
@@ -30,6 +31,37 @@ const CloudWarning = ({ data }: { data: CloudWarningType }) => {
           />
         )}
       </div>
+
+      {/* Cards Section */}
+      {data?.list?.length > 0 && (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-10">
+          {data.list.map((item, index) => (
+            <div
+              key={index}
+              className="rounded-[26px] border border-[#B8B2FF] bg-white p-6 md:p-7 min-h-[259px] transition-all duration-300"
+            >
+              {/* Card Title */}
+              {item?.title && (
+                <div className="min-h-[72px]">
+                  <DangerousHtml
+                    as="h3"
+                    html={item.title}
+                    className="text-[#2AAC94] text-[26px] leading-7.5 font-semibold line-clamp-2 [&_p]:line-clamp-2"
+                  />
+                </div>
+              )}
+
+              {/* Card Description */}
+              {item?.description && (
+                <DangerousHtml
+                  html={item.description}
+                  className="text-[#1B1B1B] text-[20px] leading-7.5"
+                />
+              )}
+            </div>
+          ))}
+        </div>
+      )}
     </section>
   );
 };
